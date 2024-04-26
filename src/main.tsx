6427e44f837a1bb1a87ba681f98dff083e987c90
marc3lsk <marc3lsk@gmail.com>
2024-04-26 14:31:20 +0200

    router suspense refaktoring

--- src/main.tsx
+++ src/main.tsx
@@ -4,29 +4,27 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import MainLayout from "./layout/MainLayout.tsx";
 import Loading from "./layout/Loading.tsx";
 import "./index.css";
+
 const Index = lazy(() => import("./pages/Index"));
 const Page2 = lazy(() => import("./pages/Page2.tsx"));
 
 const router = createBrowserRouter([
   {
     path: "/",
-    Component: MainLayout,
+    element: (
+      <Suspense fallback={<Loading />}>
+        <MainLayout />
+      </Suspense>
+    ),
+
     children: [
       {
         index: true,
-        element: (
-          <Suspense fallback={<Loading />}>
-            <Index />
-          </Suspense>
-        ),
+        element: <Index />,
       },
       {
         path: "page2",
-        element: (
-          <Suspense fallback={<Loading />}>
-            <Page2 />
-          </Suspense>
-        ),
+        element: <Page2 />,
       },
     ],
   },
